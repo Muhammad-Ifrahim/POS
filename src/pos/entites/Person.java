7a1213a8a72409586b82097275f9c49ef8252e5f
Admin <Admin@Work3-pc>
2018-03-06 12:14:19 +0500

    Person-Getter Setter 06-03-2018

--- src/pos/entites/Person.java
+++ src/pos/entites/Person.java
@@ -15,13 +15,48 @@ import com.j256.ormlite.table.DatabaseTable;
 @DatabaseTable(tableName = "Person")
 public class Person {
 
-	@DatabaseField(generatedIdSequence = "Person-", columnName = "personId")
+	@DatabaseField(generatedIdSequence = "Person-", columnName = "personId" , useGetSet = true)
 	private int id;
 
-	@DatabaseField(columnName = "firstName", dataType = DataType.STRING)
+	@DatabaseField(columnName = "firstName", dataType = DataType.STRING , useGetSet = true)
 	private String firstName;
 
-	@DatabaseField(columnName = "lastName", dataType = DataType.STRING, canBeNull = false)
+	@DatabaseField(columnName = "lastName", dataType = DataType.STRING , canBeNull = false, throwIfNull=true , useGetSet = true)
 	private String lastName;
+	
+	@DatabaseField(columnName = "address" , dataType = DataType.STRING , useGetSet = true)
+	private String address;
+
+	public Person(){
+	
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public int getId() {
+		return id;
+	}
 
 }
