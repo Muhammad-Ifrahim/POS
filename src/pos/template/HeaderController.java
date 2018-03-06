68c0e9ea3033b86977b81ad8ba9280ff28fabd66
Admin <Admin@Work3-pc>
2018-03-06 11:26:52 +0500

    Master Branch - 06/03/2018

--- src/pos/template/HeaderController.java
+++ src/pos/template/HeaderController.java
@@ -6,6 +6,9 @@ import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 
@@ -13,13 +16,17 @@ public class HeaderController {
 	
 	@FXML
 	private HBox header;
+	@FXML
+	private Button product;
 	
 	@FXML
-	private void sendProductFxml(Event e) throws IOException 
+	private void sendProductFxml(MouseEvent e) throws IOException 
 	{
 	
-		System.out.println("Hello - " + e);
-		TemplateController.mainTemplate.setCenter((BorderPane) FXMLLoader.load(getClass().getResource("Test.fxml")));
+		//System.out.println("Hello - " + product.getId()+ "---"+ product.getAccessibleText()+ e.getSource() );
+		Button button= (Button) e.getSource();
+		TemplateController.mainTemplate.setCenter((BorderPane) FXMLLoader.load(getClass().getResource(button.getId()+".fxml")));
+		
 		//  CenterController.setCenterView(new String("Test.fxml"));
 		//CenterController object=null;
 		//object.setCenterView("dsdsd");
